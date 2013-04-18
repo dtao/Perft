0dff1bd5874db6a8cc17825c6c9312c2ed98324e
Dan Tao <daniel.tao@gmail.com>
2013-04-18 08:19:37 -0700

    fixed bad column name in PerformanceTestSuiteRun model

--- web/app/models/performance_test_suite_run.rb
+++ web/app/models/performance_test_suite_run.rb
@@ -4,8 +4,8 @@ class PerformanceTestSuiteRun
   belongs_to :performance_test_suite
   has n, :performance_test_runs
 
-  property :id,                     Serial
-  property :performance_test_suite, Integer
-  property :created_at,             DateTime
-  property :updated_at,             DateTime
+  property :id,                        Serial
+  property :performance_test_suite_id, Integer
+  property :created_at,                DateTime
+  property :updated_at,                DateTime
 end
