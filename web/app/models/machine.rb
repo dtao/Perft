8d03ad4cf1ec16cb12b96755ee6595e43962be97
Dan Tao <daniel.tao@gmail.com>
2013-04-18 08:19:52 -0700

    added Machine model

---
+++ web/app/models/machine.rb
@@ -0,0 +1,19 @@
+class Machine
+  include DataMapper::Resource
+
+  property :id,         Serial
+  property :name,       String
+  property :api_key,    String
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  ALPHANUMERIC = (
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+    "abcdefghijklmnopqrstuvwxyz" +
+    "0123456789"
+  ).freeze
+
+  before :create do
+    self.api_key ||= Randy.string(64, ALPHANUMERIC)
+  end
+end
--- web/app/models/performance_test_suite_run.rb
+++ web/app/models/performance_test_suite_run.rb
@@ -1,10 +1,12 @@
 class PerformanceTestSuiteRun
   include DataMapper::Resource
 
+  belongs_to :machine
   belongs_to :performance_test_suite
   has n, :performance_test_runs
 
   property :id,                        Serial
+  property :machine_id,                Integer
   property :performance_test_suite_id, Integer
   property :created_at,                DateTime
   property :updated_at,                DateTime
