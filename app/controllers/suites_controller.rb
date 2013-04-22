a17e037caacacb2da837e0849697664e26510cb8
Dan Tao <daniel.tao@gmail.com>
2013-04-22 16:29:09 -0700

    added the ability to compare test runs

--- app/controllers/suites_controller.rb
+++ app/controllers/suites_controller.rb
@@ -7,7 +7,9 @@ Perft::App.controllers :suites do
 
   get "/:suite_id/:run_id" do |suite_id, run_id|
     @suite = PerformanceTestSuite.get(suite_id.to_i)
-    @suite_run = @suite.performance_test_suite_runs.get(run_id.to_i)
+    @suite_run = run_id == "latest" ?
+      @suite.performance_test_suite_runs.first(:order => [:id.desc]) :
+      @suite.performance_test_suite_runs.get(run_id.to_i)
     render :"suites/run"
   end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -3,4 +3,10 @@ Perft::App.controllers :tests do
     @test = PerformanceTest.get(id.to_i)
     render :"tests/show"
   end
+
+  post "/compare" do
+    @tests = PerformanceTest.all(:id => params["include"].map(&:to_i))
+    @suite = @tests.first.performance_test_suite
+    render :"tests/compare"
+  end
 end
